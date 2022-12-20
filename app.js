ef70441e7749d1ce1fc8a9f48224d66640bdeff5
NatusyaZira <natusya.zira@gmail.com>
2022-12-20 19:22:09 +0200

    Add Success and Failure Pages

--- app.js
+++ app.js
@@ -36,19 +36,26 @@ app.post("/", function(req, res) {
 
   const options = {
     method: "POST",
-    auth: "nataliia1:876eb4956e95309bc6c7604646bbe98b-us1"
+    auth: "nataliia1:a773dc3b318e8c9ca067672b55693f9b-us1"
   }
 
   const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
     response.on("data", function(data){
       console.log(JSON.parse(data));
     })
   })
   request.write(jsonData);
   request.end();
-// console.log(firstName, lastName, email);
-});
 
+});
+app.post("/failure.html", function(req, res) {
+  res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
